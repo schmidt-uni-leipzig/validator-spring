3d895f48e6d9e25fd500cdd1510f6517ecea3372
schmidt_uni_leipzig <jschmidt@informatik.uni-leipzig.de>
2014-06-16 15:55:43 +0200

    moved List into SpELAsserList Annotation

--- src/main/java/cz/jirutka/validator/spring/SpELAssertList.java
+++ src/main/java/cz/jirutka/validator/spring/SpELAssertList.java
@@ -32,45 +32,20 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import cz.jirutka.validator.spring.SpELAssert;
+
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
 /**
- * JSR-303 constraint for validation with Spring Expression Language (SpEL).
- * 
- * @author Jakub Jirutka <jakub@jirutka.cz>
+ * Defines several <code>@SpELAssertValidator</code> annotations on the same element
+ *
+ * @see FieldMatch
  */
-@Documented
-@Retention(RUNTIME)
 @Target({METHOD, FIELD, TYPE})
-@Constraint(validatedBy = SpELAssertValidator.class)
-public @interface SpELAssert {
-
-    String message() default "{cz.jirutka.validator.spring.SpELAssert.message}";
-    Class<?>[] groups() default {};
-    Class<? extends Payload>[] payload() default {};
-
-    /**
-     * Validation expression in SpEL.
-     *
-     * @see <a href="http://static.springsource.org/spring/docs/3.0.x/reference/expressions.html">
-     *     documentation of Spring Expression Language</a>
-     */
-    String value();
-
-    /**
-     * Perform validation only if this SpEL expression evaluates to <tt>true</tt>.
-     *
-     * @see <a href="http://static.springsource.org/spring/docs/3.0.x/reference/expressions.html">
-     *     documentation of Spring Expression Language</a>
-     */
-    String applyIf() default "";
-
-    /**
-     * Classes with static methods to register as helper functions which you
-     * can call from expression as <tt>#methodName(arg1, arg2, ...)</tt>.
-     * This does not support overloading, only last registered method of each
-     * name will be used!
-     */
-    Class<?>[] helpers() default { };
+@Retention(RUNTIME)
+@Documented
+public @interface SpELAssertList
+{
+    SpELAssert[] value();
 }
